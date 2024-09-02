a337dcebc77357707c3be8e69c9895f3b1ad5bee
mojtaba nabilu <mojjtaba00@gmail.com>
2024-09-02 14:09:43 +0330

    added style scroll bar to app.scss

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,6 @@ import VueToast from "vue-toast-notification";
 const ProductsCard = () => import("../template/ProductsCard.vue");
 const UsersList = () => import("../template/UsersLists.vue");
 
-
 import routes from "./routes";
 
 Vue.use(VueRouter);
@@ -14,23 +13,10 @@ Vue.use(VueToast);
 Vue.component("product-card", ProductsCard);
 Vue.component("user-list", UsersList);
 
-/*
- * If not building with SSR mode, you can
- * directly export the Router instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Router instance.
- */
-
-export default function (/* { store, ssrContext } */) {
+export default function () {
   const Router = new VueRouter({
     scrollBehavior: () => ({ x: 0, y: 0 }),
     routes,
-
-    // Leave these as they are and change in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
     mode: process.env.VUE_ROUTER_MODE,
     base: process.env.VUE_ROUTER_BASE,
   });
